4322eb9788a940bdde2e8fc7807c88e2ca86c971
Красов Данила Игоревич <DI_Krasov@gazauto.loc>
2022-04-23 17:17:58 +0300

    Topic, User, Message models added

--- blog_project/rooms/models.py
+++ blog_project/rooms/models.py
@@ -1,14 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
-    #host =
-    #topic = 
+    host = models.ForeignKey(
+        to=User,
+        related_name='rooms',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    topic = models.ForeignKey(
+        to=Topic,
+        related_name='rooms',
+        on_delete=models.SET_NULL,
+        null=True
+    )
     name = models.CharField(max_length=150)
     description = models.TextField(null=True, blank=True)
-    # participants = 
+    # participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        related_name='messages',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    room = models.ForeignKey(
+        to=Room,
+        related_name='messages',
+        on_delete=models.CASCADE
+    )
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[:20] + '...'
